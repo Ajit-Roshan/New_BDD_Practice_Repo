5f08efab34df70aee84d7daac4f7ed2d742f0c2a
ajit <ajit4roshan@gmail.com>
2023-07-03 16:55:44 +0530

    First Commit

---
+++ src/test/java/steps/Steps.java
@@ -0,0 +1,45 @@
+package steps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+
+	WebDriver dr;
+
+	@Given("in login page {string}")
+	public void in_login_page(String string) {
+	 dr= new ChromeDriver();
+	 dr.manage().window().maximize();
+	 
+	 dr.get(string);
+	}
+
+	@Then("user enter loginid {string}")
+	public void user_enter_loginid(String string) {
+		dr.findElement(By.name("q")).sendKeys(string);
+	}
+
+	@Then("user password is fillerd {string}")
+	public void user_password_is_fillerd(String string) {
+		dr.findElement(By.name("q")).sendKeys(string);	 
+	}
+
+	@When("user hit login")
+	public void user_hit_login() {
+		dr.findElement(By.name("q")).sendKeys(Keys.RETURN);
+	}
+
+	@Then("user logged in the page")
+	public void user_logged_in_the_page() {
+		System.out.println(dr.getTitle());
+	}
+
+}
---
+++ src/test/resources/Feature/first.feature
@@ -0,0 +1,11 @@
+Feature: 
+  base first
+
+  Scenario: 
+    first scenerio
+
+    Given in login page "https://www.google.com"
+    Then user enter loginid "ajit"
+    Then user password is fillerd "roshan"
+    When user hit login
+    Then user logged in the page
